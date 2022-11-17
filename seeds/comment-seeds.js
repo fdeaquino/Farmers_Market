ecbf07746c2065707cc2e3dc4f4005b96afeecd2
RDCERP <109607056+RDCERP@users.noreply.github.com>
2022-11-17 13:58:59 -0600

    Root server.js base file. We can remove what we dont need or add to it.

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,18 +1,18 @@
 const { Comment } = require('../models');
 
 const commentdata = [
-  {
-    comment_text: 'The vegetables are fresh and the prices are reasonable.',
-    user_id: 6,
-    store_id: 1,
-    post_id: 1
+    {
+        comment_text: 'The vegetables are fresh and the prices are reasonable.',
+        user_id: 6,
+        store_id: 1,
+        post_id: 1
 
-  },
-  {
-    comment_text: 'The meat is fresh and the prices are reasonable.',
-    user_id: 7,
-    store_id: 3,
-    post_id: 2
+    },
+    {
+        comment_text: 'The meat is fresh and the prices are reasonable.',
+        user_id: 7,
+        store_id: 3,
+        post_id: 2
     },
     {
         comment_text: 'The fruits are fresh and the prices are reasonable.',
