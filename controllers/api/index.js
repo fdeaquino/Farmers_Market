7500a653362292a679d653c51f7f85581eca3227
amaragh <mercer.alecia@gmail.com>
2022-11-20 16:10:57 -0600

    add category routes for findAll, findOne and create

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const storeRoutes = require('./store-routes');
+const categoryRoutes = require('./category-routes');
 
 router.use('/users', userRoutes);   
 router.use('/stores', storeRoutes);   
+router.use('/categories', categoryRoutes);   
 
 module.exports = router;
