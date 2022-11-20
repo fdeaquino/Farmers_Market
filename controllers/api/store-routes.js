9bd375c1c2bc8c7edc12d90b1c00ca6848b59a50
amaragh <mercer.alecia@gmail.com>
2022-11-20 14:48:01 -0600

    add route for creating a new store

--- controllers/api/store-routes.js
+++ controllers/api/store-routes.js
@@ -77,4 +77,27 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+
+// create a new store
+router.post('/', (req, res) => {
+    Store.create(req.body)
+        .then((store) => {
+            if (req.body.categoryIds.length) {
+                const storeCategoryIdArr = req.body.categoryIds.map((category_id) => {
+                    return {
+                        store_id: store.id,
+                        category_id
+                    };
+                });
+                return StoreCategory.bulkCreate(storeCategoryIdArr);
+            }
+            res.status(200).json(store)
+        })
+        .then((storeCategoryIds) => res.status(200).json(storeCategoryIds))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+})
 module.exports = router;
