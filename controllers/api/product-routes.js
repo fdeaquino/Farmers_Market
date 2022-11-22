91fe4ea96cd5635b21242c4747602d2a6a910bdd
amaragh <mercer.alecia@gmail.com>
2022-11-22 17:43:35 -0600

    delete: delete product by id

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -26,7 +26,8 @@ router.get('/:id', (req, res) => {
     Product.findOne({
         where: {
             id: req.params.id
-        }, attributes: [
+        }, 
+        attributes: [
             'id',
             'product_name',
             'product_description',
@@ -55,6 +56,26 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// delete product by ID
+router.delete('/:id',(req,res) => {
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No product found with this id' });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
 
 module.exports = router;
