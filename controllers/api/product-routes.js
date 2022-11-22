8cdb67b2c588269eef3f44161d9180a758b8a5c6
amaragh <mercer.alecia@gmail.com>
2022-11-22 17:36:35 -0600

    get product by id

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -1,9 +1,32 @@
 const router = require('express').Router();
 const { Product, Store } = require('../../models');
 
+// get all products 
 router.get('/', (req, res) => {
     Product.findAll({
         attributes: [
+            'id',
+            'product_name',
+            'product_description',
+            'store_id'
+        ],
+        include: [
+            { model: Store }
+        ]
+    })
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get product by id 
+router.get('/:id', (req, res) => {
+    Product.findOne({
+        where: {
+            id: req.params.id
+        }, attributes: [
             'id',
             'product_name',
             'product_description',
