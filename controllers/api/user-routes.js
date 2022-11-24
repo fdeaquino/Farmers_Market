26c79ffe0ef8ceac28a0026ddd5fb0180dedeac8
amaragh <mercer.alecia@gmail.com>
2022-11-23 19:59:18 -0600

    confirm login route working appropriately

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -105,6 +105,7 @@ router.post('/login', (req, res) => {
                 req.session.loggedIn = true;
 
             res.json({ user: dbUserData, message: 'You are now logged in!' });
+            console.log("sgwnhrghiohohrgwiognoiwhgi")
         });
     });
 });
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -34,7 +34,7 @@ async function loginFormHandler(event) {
 
     if (username && password) {
         const response = await fetch('/api/users/login', {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify({
                 username,
                 password
@@ -43,13 +43,13 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/homepage');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
     }
 };
 
-// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
 // document.querySelector('.signup-form').addEventListener("submit", signupFormHandler);
